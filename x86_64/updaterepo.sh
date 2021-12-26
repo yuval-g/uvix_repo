7e822e2e630a0757532468abe8490daedacf378c
yuval <yuval@aquari.os>
2021-12-26 21:06:17 +0200

    Add calamares

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm uvix-repo*
-repo-add -s -n -R uvix-repo.db.tar.gz *.pkg.tar.zst
+#repo-add -s -n -R uvix-repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R uvix-repo.db.tar.gz *.pkg.tar.zst
