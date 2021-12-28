cd8e99459e40b974fbc0d5741aca86db104628a2
yuval <yuval@aquari.os>
2021-12-28 21:41:16 +0200

    Fix .db files

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm uvix-repo*
+rm uvix_repo*
 #repo-add -s -n -R uvix-repo.db.tar.gz *.pkg.tar.zst
-repo-add -n -R uvix-repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R uvix_repo.db.tar.gz *.pkg.tar.zst
